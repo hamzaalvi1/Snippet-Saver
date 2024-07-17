79e2f153a0cebee69471c1eaec03db223cc95b24
Hamza Alvi <hamzaalvi224@gmail.com>
2024-07-17 18:38:16 +0500

    add console.log

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -2,6 +2,7 @@ import { Cta } from "@/components/Cta";
 import { Header } from "@/components/Header";
 
 const HomePage = () => {
+  console.log(process.env.WEBHOOK_SECRET, "WEBHOOK_SECRET");
   return (
     <main className="bg-slate-50">
       <Header />
